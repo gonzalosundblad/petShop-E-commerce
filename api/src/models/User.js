22d5be73ab07849a26cf3f1ff72cf2f0cfa30f81
Eric <eric.gomez.unq@gmail.com>
2020-10-07 16:28:42 -0300

    tipeo

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -6,24 +6,24 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     // defino el modelo
-     sequelize.define('user', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true
+    sequelize.define('user', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
         }
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
     }
-}
 )};
 
 
