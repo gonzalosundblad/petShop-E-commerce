b02055ee098197212d4c51ef4ed57d079e79e805
Eric <eric.gomez.unq@gmail.com>
2020-10-10 19:01:11 -0300

    Back-End Terminadisimo( por ahora )

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,8 +1,7 @@
 const server = require('express').Router();
-const { Product, Category } = require('../db.js');
+const { Product, Category, productcategory } = require('../db.js');
 
 server.get('/', (req, res, next) => {  //TRAE TODOS LOS PRODUCTOS
-
 	Product.findAll()
 		.then(products => {
 			res.json(products);
@@ -10,54 +9,45 @@ server.get('/', (req, res, next) => {  //TRAE TODOS LOS PRODUCTOS
 		.catch(next);
 });
 
-// server.get('/category/:nombreCat', (req, res, next) => {  //TRAE TODOS LOS PRODUCTOS DE X CATEGORIA 
+server.get('/category/:nombreCat', (req, res, next) => {  //TRAE TODOS LOS PRODUCTOS DE X CATEGORIA
+	const nombreCat = req.params.nombreCat
+	Category.findAll({
+		include : {
+			model : Product
+		},
+		where : {
+			name : nombreCat
+		}
+	})
+	.then(data => {
+		res.json(data[0].products);
+	})
+})
 
-// 	Category.findOne({
-// 		where: {
-// 			name: req.params.nombreCat
-// 		}
-// 	}).then(cat => {
-// 		return cat.id
-// 	}).then(loquellega => {
-// 		Product.findAll({
-// 		where: {
-// 			categoryId: loquellega
-// 		}
-// 	}).then(cat => {
-// 		res.json(cat)
-// 	}).catch(function(err){
-// 		res.send('error')
-// 		console.log('error: ', err)
-// 		})
-// 	})	
-// })
-
-
-// server.get('/:id', (req, res) => {			//TRAE EL PRODUCTO DEL CORRESPONDIENTE ID
-
-// 	var promiseProduct = Product.findByPk(req.params.id)
-// 	var promiseCategory = Category.findAll();
-
-// 	Promise.all([promiseProduct, promiseCategory])
-// 	.then(function(values){
-// 		var product = values[0];
-// 	  		var lista = values[1];
-// 		  	var ele = lista.find(element => element.id === product.categoryId);
-// 		  	var objeto = {
-// 				  name : product.name,
-// 				description : product.description,
-// 				price: product.price,
-// 				stock: product.stock,
-// 			  	categoria : ele.name,
-// 			}
-// 		res.json(objeto)
-// 	}).catch(err => res.status(404).send('Producto no encontrado'))
-// })
+server.get('/:id', (req, res) => {			//TRAE EL PRODUCTO DEL CORRESPONDIENTE ID
+	var arr = [];
+	Product.findByPk(req.params.id, {
+		include: {
+			model: Category
+		}
+	}).then(function(data) {
+		for (let i = 0; i < data.categories.length; i++) {
+			arr.push(data.categories[i].name)
+		}
+		console.log(data)
+		res.json({
+			name: data.name,
+			description: data.description,
+			price: data.price,
+			stock: data.stock,
+			image: data.image,
+			categories: arr
+		})
+	})
+})
 
 server.post('/', (req, res) => {		//AGREGA NUEVOS PRODUCTOS
-
 	const {name, description, price, stock } = req.body;
-	
 	if( !name || !description ){
 		return res.status(400).send("Campos requeridos")
 	} else {
@@ -74,9 +64,7 @@ server.post('/', (req, res) => {		//AGREGA NUEVOS PRODUCTOS
 })
 	
 server.post('/category', (req, res) => {		//AGREGA NUEVAS CATEGORIAS
-
 	const { name, description } = req.body ;
-	
 	if(!name){
 		return res.status(400).send('Campos requeridos')
 	}
@@ -88,57 +76,51 @@ server.post('/category', (req, res) => {		//AGREGA NUEVAS CATEGORIAS
 	})
 })
 
-
-
-server.post('/:idProducto/category/:idCategoria', (req, res) => {
-
+server.post('/:idProducto/category/:idCategoria', (req, res) => {		//AGREGA UNA CATEGORIA A UN PRODUCTO
 	const { idProducto, idCategoria } = req.params;
 	Product.findByPk(idProducto)
 	.then(data => {
 		data.addCategories(idCategoria)
 		res.send('categoria agregada')
 	})
-
 })
 
-
-server.delete('/:idProducto/category/:idCategoria', (req, res) => {
-
+server.delete('/:idProducto/category/:idCategoria', (req, res) => {		//ELIMINA UNA CATEGORIA DE UN PRODUCTO
 	const { idProducto, idCategoria } = req.params;
 	Product.findByPk(idProducto)
 	.then(data => {
 		data.removeCategories(idCategoria)
 		res.send('categoria eliminada')
 	})
-
 })
 
-
-server.put('/:id', function(req, res, next) {		//MODIFICA UN PRODUCTO SEGUN SU ID
-
-	const {name, description, price, stock } = req.body;
-
-	Product.update({
-		name,
-		description,
-		price,
-		stock
-	},{
-		returning: true,
-		where: {
-			id: req.params.id
-		}
-	}).then(function(product) {
-		res.status(200).json(product)
-	}).catch(err => {
-		res.status(400)
-	})
+server.put('/:id', function(req, res, next) {       //MODIFICA UN PRODUCTO SEGUN SU ID
+    const {name, description, price, stock } = req.body;
+    Product.update({
+        name,
+        description,
+        price,
+        stock
+    },{
+        returning: true,
+        where: {
+            id: req.params.id
+        }
+    }).then(function(product) {
+        if(product[0] == 0) {
+        return product[0]
+        }
+        res.status(200).json(product)
+    }).then(err => {
+        res.status(400).send('Error, campos requeridos')
+    }).catch(err => {
+        res.status(400)
+        console.log('Error: ', err)
+    })
 });
 
 server.put('/category/:id', function(req, res, next) {		//MODIFICA UNA CATEGORIA SEGUN ID
-
 	const {name, description} = req.body;
-	
 	Category.update({
 		name,
 		description
@@ -154,9 +136,7 @@ server.put('/category/:id', function(req, res, next) {		//MODIFICA UNA CATEGORIA
 });
 
 server.delete('/:id', (req, res) => {		//ELIMINA UN PRODUCTO SEGUN ID
-
 	var productId = req.params.id;
-
 	if(!productId){
 		res.status(404).send('Debes ingresar un ID')
 	} else {
@@ -170,13 +150,8 @@ server.delete('/:id', (req, res) => {		//ELIMINA UN PRODUCTO SEGUN ID
 			})
 	}
 })
-
-
-
 server.delete('/category/:id', (req, res) => {		//ELIMINA UNA CATEGORIA
-
 	var categoryId = req.params.id;
-	
     if(!categoryId){
         res.status(404).send('Debes ingresar un ID')
     } else {
@@ -190,7 +165,5 @@ server.delete('/category/:id', (req, res) => {		//ELIMINA UNA CATEGORIA
             })
     }
 });
-
-
 
 module.exports = server;
