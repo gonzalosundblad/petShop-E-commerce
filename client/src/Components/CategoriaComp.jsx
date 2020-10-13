c51bd00c75c6f2989dd10031e483c930ed29e104
narela <narela315@gmail.com>
2020-10-13 06:51:20 -0300

    EsLoQueHay

--- client/src/Components/CategoriaComp.jsx
+++ client/src/Components/CategoriaComp.jsx
@@ -2,11 +2,13 @@ import React  from 'react';
 import { Link } from 'react-router-dom';
 import  '../Estilos/productCard.css';
 import  '../imagenes/perros.jpg';
+import axios from 'axios';
+import borrarCategoria from '../Containers/BorrarCategoria';
 
-export default function CategoriaCard ({nombre}){
-    // const [card, setCard] = useState()
- 
+export default function CategoriaCard ({nombre, id}){
+
     return(
+      <div>
       <Link to={`/products/${nombre}`}>
         <div className="card">
             {/* <figure>
@@ -14,9 +16,14 @@ export default function CategoriaCard ({nombre}){
             </figure> */}
             <h1 className="card-title">Categoria</h1>
             <h1 className="card-title">{nombre}</h1>
+            
         </div>
     </Link>
+    <button onClick={borrarCategoria(id)}>Borrar</button>
+    </div>
     )
       
       
   };
+
+
