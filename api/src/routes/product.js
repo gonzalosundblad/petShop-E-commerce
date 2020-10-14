b2b81141a10feffda38cee4c3dedb75ae32d3c30
gonzalo <sundbladgonzalo@gmail.com>
2020-10-14 20:17:32 -0300

    imagenes en nube pero no conectadas a base de datos

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -64,11 +64,18 @@ server.get('/:id', (req, res) => {			//TRAE EL PRODUCTO DEL CORRESPONDIENTE ID
 })
 
 server.post('/', (req, res) => {		//AGREGA NUEVOS PRODUCTOS
-	const {name, description, price, stock, categoryId } = req.body;
+	const {name, description, price, stock, categoryId, image} = req.body;
+	console.log(req.body)
 	if( !name || !description ){
 		return res.status(400).send("Nombre y descripcion son requeridos")
 	} else if(!categoryId) {
-		Product.create({ name, description, price, stock })
+		Product.create({ 
+			name, 
+			description, 
+			price, 
+			stock, 
+			image: `https://firebasestorage.googleapis.com/v0/b/petshopfiles.appspot.com/o/fotosProductos%2F${image.slice(12)}?alt=media&token=${tokenDeAccesoArchivoFirebase}`
+		 })
 			.then(function(productSinId) {
 				productSinId.addCategories("0")
 				res.json(productSinId)
@@ -79,7 +86,13 @@ server.post('/', (req, res) => {		//AGREGA NUEVOS PRODUCTOS
 				id: categoryId
 			}
 		})
-		var producto = Product.create({ name, description, price, stock })
+		var producto = Product.create({ 
+			name, 
+			description, 
+			price, 
+			stock, 
+			image: `https://firebasestorage.googleapis.com/v0/b/petshopfiles.appspot.com/o/fotosProductos%2F${image.slice(12)}?alt=media&token` 
+		 })
 		Promise.all([category, producto])
 			.then(values => {
 				var category = values[0]
