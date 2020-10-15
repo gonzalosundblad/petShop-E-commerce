2ddf7d1b261887e68ada216fb15e1b7a49af2d24
Joana Faro <joana11297@gmail.com>
2020-10-14 23:41:55 -0300

    actualizando

--- client/src/Components/MultipleSelect.jsx
+++ client/src/Components/MultipleSelect.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import ListItemText from '@material-ui/core/ListItemText';
-import Select from '@material-ui/core/Select';
-import Checkbox from '@material-ui/core/Checkbox';
-import Chip from '@material-ui/core/Chip';
+import Input from '@material-ui/core/input';
+import InputLabel from '@material-ui/core/inputLabel';
+import MenuItem from '@material-ui/core/menuItem';
+import FormControl from '@material-ui/core/formControl';
+import ListItemText from '@material-ui/core/listItemText';
+import Select from '@material-ui/core/select';
+import Checkbox from '@material-ui/core/checkbox';
+// import Chip from '@material-ui/core/Chip';
 import { Button } from '@material-ui/core';
 import axios from 'axios';
 
