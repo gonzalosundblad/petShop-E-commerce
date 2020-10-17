4c94da46588e732e9f72ce0d444d0d31b64f7628
Joana Faro <joana11297@gmail.com>
2020-10-17 17:08:14 -0300

    intentando mostrar mas de 1 producto

--- client/src/Components/ListaDesplegable.jsx
+++ client/src/Components/ListaDesplegable.jsx
@@ -21,9 +21,9 @@ export function ListaDesplegable(){
                         </ul>
                     </li>
                     <li><a href=''>Ordenes</a>
-                    <ul>
-                            <li><a href='/admin/AgregarCategoria'>Agregar Categorias</a></li>
-                            <li><a href='/admin/ModificarCategoria/'>Modificar/Borrar</a></li>
+                    <ul className="orden">
+                            <li><a href='/admin/ordenes'>Ordenes</a></li>
+                            
                         </ul>
                     </li>
                 </ul>
