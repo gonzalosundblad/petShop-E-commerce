714711a934a846645d4a12801e05d97126ab9377
gabbies86 <gabrielamarciano054@gmail.com>
2020-10-20 12:45:14 -0300

    gaby

--- client/src/Containers/Catalogo.jsx
+++ client/src/Containers/Catalogo.jsx
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import Catalogo from '../Components/CatalogoComp'
-import {getProducts} from '../Redux/actions';
+import {getProductsRequest} from '../Redux/actions';
+import {connect} from 'react-redux'
 
-export default function MostrarCatalogo () {
-
-  const [products, setProducts] = useState([]);
-
+function MostrarCatalogo ({products, getProductsRequest}) {
   useEffect(() => {
-    getProducts().payload
-    .then(resp => setProducts(resp.data))
+    getProductsRequest()
+    console.log('useEffect');
+    console.log(products);
   }, []);
-    
+
   return (
     <div >
       <Catalogo productos = {products} />
     </div>
-
   );
 };
+const mapStateToProps = state => {
+  return {
+    products: state.products
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    getProductsRequest: () => dispatch(getProductsRequest())
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MostrarCatalogo)
