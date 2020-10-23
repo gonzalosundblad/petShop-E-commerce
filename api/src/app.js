d2a2dc6569a39d122f144db351846518bc825e14
Joana Faro <joana11297@gmail.com>
2020-10-23 18:38:18 -0300

    modificando passport y login

--- api/src/app.js
+++ api/src/app.js
@@ -5,13 +5,75 @@ const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const passport = require('passport');
 const session = require('express-session');
-const flash = require('express-flash')
+const LocalStrategy = require("passport-local").Strategy;
+const { User } = require('./db');
+const bcrypt = require("bcrypt");
 
 
 require('./db.js');
 
 const server = express();
 server.name = 'API';
+
+
+const authenticateUser = (email, password, done) => {
+  User.findOne({ where: { email: email } })
+    .then(user => {
+      if (!user) {
+        return done(null, false, {
+          message: 'El correo electrónico no existe.',
+        });
+      }
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) {
+          console.log(err);
+        }
+        if (isMatch) {
+          return done(null, {
+            user_id: user.user_id,
+            name: user.name,
+            last_name: user.last_name,
+            role: user.role,
+            email: user.email
+          });
+        } else {
+          return done(null, false, { message: "Contraseña Incorrecta" });
+        }
+      });
+
+    })
+    .catch(err => {
+      if (err) {
+        console.log('error en Servidor');
+        return done(err);
+      }
+    });
+}
+
+
+passport.use(new LocalStrategy(
+  { usernameField: "email", passwordField: "password" },
+  authenticateUser
+));
+
+passport.serializeUser((user, done) => done(null, user.user_id));
+
+// passport.deserializeUser((id, done) => {
+//   User.findOne({ where: { id: id } })
+//     .then(user => {
+//       if (user) {
+//         return done(null, user);
+//       }
+//     })
+//     .catch(err => {
+//       if (err) {
+//         console.log('error');
+//         return done(err);
+//       }
+//     })
+// })
+
+
 
 server.use(session({
   secret: 'secret',
@@ -20,8 +82,8 @@ server.use(session({
 }))
 
 server.use(passport.initialize())
-server.use(flash())
-require('./passport')(passport);
+// require('./passport')(passport);
+server.use(passport.session())
 
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
